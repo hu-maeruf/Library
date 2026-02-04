a7f2efd4ef6cf7a0dac377ae2fd8c5b4492f919f
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2026-02-04 16:28:31 +0800

    Add a function that add book from user input

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function Book(title, author, pageNumber, bookStatus) {
   this.pageNumber = pageNumber;
   this.bookStatus = bookStatus;
 }
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", addBookToLibrary)
+
+function addBookToLibrary() {
+  const bookTitle = document.getElementById("title").value;
+  const bookAuthor = document.getElementById("author").value;
+  const bookPages = document.getElementById("pageNumber").value;
+  let selectedRadio = document.querySelector("input[name='bookStatus']:checked");
+  let bookStatus;
+  if (selectedRadio === null) {
+    bookStatus = "No";
+  } else {
+    bookStatus = selectedRadio.value;
+  }
+  const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+  myLibrary.push(newBook);
+}
